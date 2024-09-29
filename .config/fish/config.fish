e275cd1727254869254b35dda42fea0bb3c990f6
hvstuff <hvstuff4@proton.me>
2024-09-29 20:16:58 +0700

    initial fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    alias hx='helix'
+    alias m='make'
+    alias ls='exa'
+end
+
+if status is-login
+    export GTK_THEME=Adwaita:dark
+end
+
+# Created by `pipx` on 2024-09-28 03:51:54
+set PATH $PATH /home/hv/.local/bin
