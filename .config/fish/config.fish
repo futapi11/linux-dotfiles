6e826ca03c9404c39b40b9a70292a9ca27764e86
hvstuff <hvstuff4@proton.me>
2024-10-10 21:39:26 +0700

    fish/config.fish: change to rose pine

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     alias hx='helix'
     alias m='make'
     alias ls='exa'
+
+    bash -c "export EDITOR=helix"
+    fish_config theme choose "Rosé Pine"
 end
 
 if status is-login
@@ -10,3 +13,10 @@ end
 
 # Created by `pipx` on 2024-09-28 03:51:54
 set PATH $PATH /home/hv/.local/bin
+
+# pnpm
+set -gx PNPM_HOME "/home/hv/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
