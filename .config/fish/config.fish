900cc28688bfb3a6ba7d601d183b737e9db98279
hvstuff <hvstuff4@proton.me>
2024-10-23 14:36:00 +0700

    direnv test

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,11 @@
 if status is-interactive
-    alias hx='helix'
     alias m='make'
-    alias ls='exa'
+    alias ls='exa --icons=always'
 
-    bash -c "export EDITOR=helix"
-    fish_config theme choose "Rosé Pine"
+    #bash -c "export EDITOR=nvim -u NORC"
 end
 
 if status is-login
-    export GTK_THEME=Adwaita:dark
 end
 
 # Created by `pipx` on 2024-09-28 03:51:54
@@ -17,6 +14,8 @@ set PATH $PATH /home/hv/.local/bin
 # pnpm
 set -gx PNPM_HOME "/home/hv/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-    set -gx PATH "$PNPM_HOME" $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+direnv hook fish | source
