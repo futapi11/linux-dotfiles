2a00cbb24a26310d1d98376bb052ab2bc67ed9f9
hvstuff4 <hvstuff4@proton.me>
2024-11-19 15:35:13 +0700

    add some stuff

---
+++ files/setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+xdg-user-dirs-update --set MUSIC ~/mus
+xdg-user-dirs-update --set VIDEOS ~/vid
+xdg-user-dirs-update --set DESKTOP ~   
+xdg-user-dirs-update --set DOWNLOAD ~/dl
+xdg-user-dirs-update --set PICTURES ~/img
+xdg-user-dirs-update --set TEMPLATES ~   
+xdg-user-dirs-update --set PUBLICSHARE ~
+xdg-user-dirs-update --set DOCUMENTS ~/doc
